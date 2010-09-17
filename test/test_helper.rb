64c7556f86935a7bd5d8f5bbc4bf3a3643b7ea2d
netarc <josh.hollenbeck@citrusbyte.com>
2010-09-16 19:51:10 -0700

    added test_helper

---
+++ test/test_helper.rb
@@ -0,0 +1,17 @@
+# Add this folder to the load path for "test_helper"
+$:.unshift(File.dirname(__FILE__))
+
+require 'byte_buffer'
+require 'contest'
+require 'mocha'
+
+# Try to load ruby debug since its useful if it is available.
+# But not a big deal if its not available (probably on a non-MRI
+# platform)
+begin
+  require 'ruby-debug'
+rescue LoadError
+end
+
+class Test::Unit::TestCase
+end
