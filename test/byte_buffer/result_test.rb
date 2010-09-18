a61a109f476c9ab8dccbba9e57be488f16ba5df4
netarc <josh.hollenbeck@citrusbyte.com>
2010-09-18 15:04:21 -0700

    updates to result tests

--- test/byte_buffer/result_test.rb
+++ test/byte_buffer/result_test.rb
@@ -31,12 +31,29 @@ class ResultTest < Test::Unit::TestCase
       result = ByteBuffer::Result.new "FOOBAR"
 
       assert_equal "FOO", result.to_s(:bits => 24)
+      assert_equal "FOOB", result.to_s(:bits => 25)
 
       assert_equal 2, result.to_i(:bits => 2)
       assert_equal 6, result.to_i(:bits => 4)
       assert_equal 70, result.to_i(:bits => 8)
+      assert_equal 591, result.to_i(:bits => 10)
       assert_equal 20294, result.to_i(:bits => 16)
       assert_equal 1112493894, result.to_i(:bits => 32)
+    end
+
+    should "specify endian to read" do
+      result = ByteBuffer::Result.new "FOOBAR!"
+
+      assert_equal "FOOBAR!", result.to_s(:endian => :little_endian)
+      assert_equal "FOOBAR!", result.to_s(:endian => :big_endian)
+
+      assert_equal 9379114470297414, result.to_i(:endian => :little_endian)
+      assert_equal 19790450202333729, result.to_i(:endian => :big_endian)
+
+      result = ByteBuffer::Result.new "\242\325$\323\374\260(@"
+
+      assert_equal 12.3456789, result.to_f(:endian => :little_endian)
+      assert_in_delta -6.93563621441161e-141, result.to_f(:endian => :big_endian), 0.1e-141
     end
   end
 end
